b5b8d70151678f13fc0671382e5bf8b1b44d6001
maxorax <orlov3020max@gmail.com>
2021-06-22 16:47:29 +0400

    update viewController

--- ContactList/ViewController.swift
+++ ContactList/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello world!")
         // Do any additional setup after loading the view.
     }
 

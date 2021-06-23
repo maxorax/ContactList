723c74be8df0cc22c486347f086afc7575a250b0
maxorax <orlov3020max@gmail.com>
2021-06-23 15:40:56 +0400

    launch xib

--- ContactList/AppDelegate.swift
+++ ContactList/AppDelegate.swift
@@ -13,8 +13,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        print("width = \(UIScreen.main.bounds.width)")
+        print("height = \(UIScreen.main.bounds.height)")
+
         // Override point for customization after application launch.
+       // let nib: UINib = UINib(nibName: "ViewController", bundle: nil)
+        let vc = ViewController()
         
+        self.window?.rootViewController = vc
+        UIView.transition(
+            with: self.window!,
+                    duration: 0,
+                    options: .transitionCrossDissolve,
+                    animations: nil,
+                    completion: nil
+                )
+        
+
+        self.window?.makeKeyAndVisible()
         return true
     }
 
